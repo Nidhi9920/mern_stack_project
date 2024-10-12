3730f47f25bbc0677c100aa8c885ebec6cb8f92f
“Nidhi” <nidhi9999333@gmail.com>
2024-10-12 21:41:56 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import postRoutes from './routes/posts.js'
+
+const app = express()
+app.use('/posts',postRoutes)
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+app.use(cors())
+
+const CONNECTION_URL = "mongodb+srv://nidhi9999333:e5oLkXzeGShFRvbN@cluster0.llmpk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const PORT = process.env.PORT|| 5002;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+//mongoose.set('useFindAndModify', false);
